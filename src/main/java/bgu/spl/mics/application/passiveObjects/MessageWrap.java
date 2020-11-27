c1ed9c1751f8547a171afb31eb01b64fb2ef4af1
max <maximgel@post.bgu.ac.il>
2020-11-27 18:19:46 +0200

    MessageBusImpl updates v1.0 MessageWrap changed list type to ArrayList from LinkedList MicroService changed variable name of Message from 'm' to 'message'

--- src/main/java/bgu/spl/mics/application/passiveObjects/MessageWrap.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/MessageWrap.java
@@ -4,17 +4,17 @@ import bgu.spl.mics.Event;
 import bgu.spl.mics.Message;
 import bgu.spl.mics.MicroService;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MessageWrap implements Message {
     private static int roundRobinCounter;
     private Class<? extends Message> messageType;
-    private List<MicroService> subscribedMS; // this lisy contains the subscribed MS for this type
+    private List<MicroService> subscribedMS; // this list contains the subscribed MS for this type
 
     public MessageWrap(Class<? extends Message> messageType, int counterInit){
         this.messageType = messageType;
-        subscribedMS = new LinkedList<>();
+        subscribedMS = new ArrayList<>();
         roundRobinCounter = counterInit;
     }
     public void addMS(MicroService ms){
